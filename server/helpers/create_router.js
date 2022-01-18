e063bc7040a380ee747004d866202b4e3e20a20b
Jamie <jamienolan93@hotmail.co.uk>
2022-01-18 15:17:09 +0000

    Ensured that current code works

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,20 +1,30 @@
 const express = require('express');
+const { response, request } = require('express');
+const ObjectID = require('mongodb').ObjectID;
 
 const createRouter = function (collection) {
+
+  const router = express.Router();
 
   router.get('/', (req, res) => {
     collection
       .find()
       .toArray()
-      .then((docs) => res.json(docs))
+      .then((docs) => {res.json(docs)})
+      .catch((error) => {
+        console.error(error);
+        response.status(500);
+        response.json({status:500, error: error})
+      })
   });
 
   router.get('/:id', (req, res) => {
     collection
       .findOne({ _id: ObjectID(id) })
-      .then((doc) => res.json(doc))
+      .then((doc) => {res.json(doc)})
   });
 
+    return router;
 };
 
 module.exports = createRouter;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js')
 const cors = require('cors');
 
-MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true })
+app.use(cors());
+app.use(express.json());
+
+MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
   .then((client) => {
     const db = client.db('birds');
     const sightingsCollection = db.collection('sightings');
